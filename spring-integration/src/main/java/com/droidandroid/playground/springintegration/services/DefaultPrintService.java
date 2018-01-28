e0dd418e48cad8e69e287e794d6cfddd0fcae5ef
Oscar Doral <oscardo@amdocs.com>
2018-01-28 20:44:03 +0100

    recipient-list-router

---
+++ spring-integration/src/main/java/com/droidandroid/playground/springintegration/services/DefaultPrintService.java
@@ -0,0 +1,9 @@
+package com.droidandroid.playground.springintegration.services;
+
+public class DefaultPrintService {
+
+    public void print(String message) {
+        System.out.println("Printing from the default service: "+message);
+    }
+
+}
