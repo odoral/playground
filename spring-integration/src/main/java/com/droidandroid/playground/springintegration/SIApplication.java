46b729feb97821957621ad78c5cd48b31be91c4e
Oscar Doral <oscardo@amdocs.com>
2018-01-31 01:00:31 +0100

    Filters

--- spring-integration/src/main/java/com/droidandroid/playground/springintegration/SIApplication.java
+++ spring-integration/src/main/java/com/droidandroid/playground/springintegration/SIApplication.java
@@ -26,7 +26,7 @@ public class SIApplication implements ApplicationRunner{
     public void run(ApplicationArguments args) throws Exception {
 
         for (int i=0;i<10;i++){
-            Message<?> message = MessageBuilder.withPayload(i % 2 == 0 ? "Printing message payload for " + i : i)
+            Message<?> message = MessageBuilder.withPayload(i % 2 != 0 ? "Printing message payload for " + i : i)
                     .setHeader("messageNumber", i)
                     .build();
             System.out.println("Sending message "+i);
